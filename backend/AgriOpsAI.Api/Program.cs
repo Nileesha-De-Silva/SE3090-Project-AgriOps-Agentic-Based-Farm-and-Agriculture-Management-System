5ba01bedfd87ce8e75b2260200b4e279c1195c7a
dinalisewmini <dinalisewmini21@gmail.com>
2026-09-23 15:18:52 +0530

    chore(backend): upgrade to .NET 10

--- backend/AgriOpsAI.Api/Program.cs
+++ backend/AgriOpsAI.Api/Program.cs
@@ -42,8 +42,8 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+.WithName("GetWeatherForecast");
+
 
 app.Run();
 
